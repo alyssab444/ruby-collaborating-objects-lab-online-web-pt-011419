e71b16d2fc38882c987c002b09c130ade4855cf3
Alyssa Brown <alyssa.brown@live.com>
2019-02-13 01:50:25 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,11 +3,20 @@ class MP3Importer
   def initialization(path)
     @path= path
   end 
-  def import(list_of_filenames)
-    list_of_filenames.each {|filename| Song.new_by_filename(filename)}
-  end 
-   
-  
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+end
   #parses a directory of files 
   #send the filenames to a song class 
   #creates a libray of music with artists that are unique
