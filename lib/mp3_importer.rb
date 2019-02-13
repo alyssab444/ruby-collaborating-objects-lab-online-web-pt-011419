73fe9a71e3f9d40f7398af7d3b98473637f83245
Alyssa Brown <alyssa.brown@live.com>
2019-02-13 01:52:31 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,10 @@
-class MP3Importer 
-  attr_accessor :path 
-  def initialization(path)
-    @path= path
-  end 
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
   def files
     files = []
     Dir.new(self.path).each do |file|
@@ -17,20 +19,3 @@ class MP3Importer
     end
   end
 end
-  #parses a directory of files 
-  #send the filenames to a song class 
-  #creates a libray of music with artists that are unique
-  #.files method 
-  #.import method : Song.new_by_filename(some_filename) this will send us to the Song class
-  #path attribute that gets set on initialization 
-  #google how to get a list of files in a direcotry
-  #only .mp3 files 
-  
-
-
-
-
-
-
-
-end 
