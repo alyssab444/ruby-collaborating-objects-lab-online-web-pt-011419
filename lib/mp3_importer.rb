a3ee151dcb6a5ef146ebd4d419741dabdf28aefb
Alyssa Brown <alyssa.brown@live.com>
2019-02-13 01:03:55 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,12 @@
 class MP3Importer 
+  #parses a directory of files 
+  #send the filenames to a song class 
+  #creates a libray of music with artists that are unique
+  #.files method 
+  #.import method : Song.new_by_filename(some_filename) this will send us to the Song class
+  #path attribute that gets set on initialization 
+  #google how to get a list of files in a direcotry
+  #only .mp3 files 
   
 
 
