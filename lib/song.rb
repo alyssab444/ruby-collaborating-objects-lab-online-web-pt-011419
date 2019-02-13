772a357f0cbd3ed1f78a10f8a409ca24118fc1a4
Alyssa Brown <alyssa.brown@live.com>
2019-02-13 01:10:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 class Song 
-  
-  
+  attr_accessor :title 
+  def self.new_by_filename(filename)
+    song= self.new 
+    song.title= filename.split("-")[1]
+    song 
+  end 
+  #parse a filename to find the song name and artist name
   
   
   
